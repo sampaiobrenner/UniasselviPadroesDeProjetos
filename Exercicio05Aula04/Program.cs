f93fc6bd5cb76796d669a37b5b2b58a76ff30018
telesala <telesala@LAB1-13>
2017-09-04 21:33:10 -0300

    Commit Aula 04 - Dia 04/09/2016

---
+++ Exercicio05Aula04/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio05Aula04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcao = -1;
+            string [] nomes = new string [10];
+
+            nomes[0] = "Matheus Sampaio";
+            nomes[1] = "Daniele Sampaio";
+            nomes[2] = "Cristina Sampaio";
+            nomes[3] = "Marina Nunes";
+            nomes[4] = "Paulo Sampaio";
+            nomes[5] = "Rossana Sampaio";
+            nomes[6] = "Roberta Sampaio";
+            nomes[7] = "Nathalia Bandeira";
+            nomes[8] = "Getúlio Franco";
+            nomes[9] = "Flávio Brenner";
+
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("============== Menu ==============");
+                Console.WriteLine("1) Pesquisar Nome");
+                Console.WriteLine("2) Listar Todos Os Nomes");
+                Console.WriteLine("0) Sair do Programa");
+                Console.WriteLine("==================================");
+                Console.WriteLine();
+                try
+                {
+                    Console.Write("Opção: ");
+                    opcao = int.Parse(Console.ReadLine());
+                    Console.WriteLine();
+
+                    switch (opcao)
+                    {
+                        case 1:
+
+                            Console.Write("Digite um nome: ");
+                            Console.WriteLine();
+                            string teclado = Console.ReadLine();
+
+                            for (int i = 0; i < nomes.Length; i++)
+                            {
+                                if (nomes[i].LastIndexOf(teclado, StringComparison.OrdinalIgnoreCase) > 0)
+                                {
+                                    Console.WriteLine("Nome encontrado: " + nomes[i]);
+                                }
+                            }
+
+                            break;
+
+                        case 2:
+
+                            for (int i = 0; i < nomes.Length; i++)
+                            {
+                                Console.WriteLine("Nome encontrado: " + nomes[i]);
+                            }
+
+                            break;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("==========================================================================");
+                    Console.WriteLine();
+                    Console.WriteLine("                        Atenção! Ocorreu um erro:                         ");
+                    Console.WriteLine("\n * " + ex.Message);
+                    Console.WriteLine();
+                    Console.WriteLine("==========================================================================");
+                    Console.WriteLine();
+                }
+
+
+            } while (opcao != 0);
+           
+        }
+    }
+}
---
+++ Exercicio06Aula04/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio06Aula04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] matriz = new int[10, 5];
+            Random numero = new Random();
+            int menor;
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    matriz[i, j] = numero.Next(10, 99);
+                }
+            }
+
+            menor = matriz[0, 0];
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (matriz[i, j] < menor)
+                    {
+                        menor = matriz[i, j];
+                    }
+                }
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(matriz[i, j] + "\t"); 
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("O menor é: " + menor);
+
+            Console.ReadKey();
+        }
+    }
+}
